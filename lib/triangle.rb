c39d668cf59fb30ecaa8d129fc05e1053133420e
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-06 21:00:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,9 +18,13 @@ class Triangle
   #if any side of triangle is less than or equal to zero, raise error
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
+        rescue TriangleError => error
+          puts error.message
     #if triangle violates triangle inequality, raise error
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
+          rescue TriangleError => error
+          puts error.message
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
