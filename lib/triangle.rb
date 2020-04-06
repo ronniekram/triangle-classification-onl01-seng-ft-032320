598bab2e683deb5b07f6ce3b0e625294dc64a130
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-06 20:39:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,14 @@
 class Triangle
   attr_accessor :equilateral, :isosceles, :scalene 
   
+  #initialize triangle with sides
   def initialize(one, two, three)
     @one = one 
     @two = two 
     @three = three
   end 
   
+  #returns the type of triangle based on triangle parameters
   def kind
   end
   
